2f44e49289532bbcce2365d6652cf35083b985b6
sitiaulia <sitiaulia1092@gmail.com>
2021-11-21 11:45:58 +0700

    Pertemuan 2

--- app/src/main/java/com/sitiaulia/a211androidl/MainActivity.kt
+++ app/src/main/java/com/sitiaulia/a211androidl/MainActivity.kt
@@ -2,10 +2,33 @@ package com.sitiaulia.a211androidl
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val simpanBt = findViewById<Button>(R.id.simpan)
+        val batalBt = findViewById<Button>(R.id.batal)
+
+        val namaEt = findViewById<EditText>(R.id.et_nama)
+        val emailEt = findViewById<EditText>(R.id.et_email)
+        val phoneEt = findViewById<EditText>(R.id.et_phone)
+        val lahirEt = findViewById<EditText>(R.id.et_lahir)
+        val sksEt = findViewById<EditText>(R.id.et_sks)
+
+        simpanBt.setOnClickListener {
+            val nama = namaEt.text.toString()
+            var sks = sksEt.text.toString().toInt()
+            sks = 160 - sks
+            Toast.makeText(this, "Sisa SKS... $sks", Toast.LENGTH_SHORT).show()
+        }
+
+        batalBt.setOnClickListener {
+            Toast.makeText(this, "batal...", Toast.LENGTH_SHORT).show()
+        }
     }
 }
